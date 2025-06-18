7dc78f805c8a27c0f061cff50b246c97886960ed
MALAPAKA DEEPAK <deepakmalapakadvns@gmail.com>
2025-06-18 09:50:08 +0530

    Spotify final commit

---
+++ JS/scrpit.js
@@ -0,0 +1,149 @@
+console.log("Let's start JavaScript");
+let currentSong = new Audio();
+let songs;
+let currFolder;
+function secondsToMinutesSeconds(seconds) {
+  if (isNaN(seconds) || seconds < 0) {
+    return "00:00";
+  }
+
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = Math.floor(seconds % 60);
+
+  const formattedMinutes = String(minutes).padStart(2, "0");
+  const formattedSeconds = String(remainingSeconds).padStart(2, "0");
+
+  return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+async function getSongs() {
+  let a = await fetch("http://127.0.0.1:5500/songs/");
+  let response = await a.text();
+  console.log(response);
+  let div = document.createElement("div");
+  div.innerHTML = response;
+  let as = div.getElementsByTagName("a");
+  console.log(as);
+  songs = [];
+  for (let index = 0; index < as.length; index++) {
+    const element = as[index];
+    if (element.href.endsWith(".mp3")) {
+      songs.push(element.href.split("/songs/")[1]);
+    }
+  }
+  return songs;
+}
+const playMusic = (track, pause = false) => {
+  currentSong.src = "/songs/" + track;
+  if (!pause) {
+    currentSong.play();
+    play.src = "/assets/images/pause.svg";
+  }
+  document.querySelector(".songInfo").innerHTML = decodeURI(track);
+  document.querySelector(".songTime").innerHTML = "00:00 / 00:00";
+};
+async function main() {
+  // get the list of all the songs
+  let songs = await getSongs();
+  console.log(songs);
+  playMusic(songs[0], true);
+  let songUL = document
+    .querySelector(".songList")
+    .getElementsByTagName("ul")[0];
+  for (const song of songs) {
+    songUL.innerHTML =
+      songUL.innerHTML +
+      `<li><img src="/assets/images/music.svg" alt="music" class="filter">
+                            <div class="info">
+                                <div style="padding:5px 0px"> ${
+                                  song.replaceAll("%20", " ").split("-")[0]
+                                } </div>
+                                <div style="padding:5px 0px">Anirudh Ravichander</div>
+                            </div>
+                            <div class="playNow">
+                                <span>Play Now</span>
+                                <img src="/assets/images/play.svg" alt="play" class="filter">
+                            </div>
+                        </li>`;
+  }
+  Array.from(
+    document.querySelector(".songList").getElementsByTagName("li")
+  ).forEach((e) => {
+    e.addEventListener("click", (element) => {
+      playMusic(e.querySelector(".info").firstElementChild.innerHTML.trim());
+    });
+  });
+
+  //AddEvent listner to play ,next and previous buttons
+  play.addEventListener("click", () => {
+    if (currentSong.paused) {
+      currentSong.play();
+      play.src = "/assets/images/pause.svg";
+    } else {
+      currentSong.pause();
+      play.src = "/assets/images/play.svg";
+    }
+  });
+
+  currentSong.addEventListener("timeupdate", () => {
+    console.log(currentSong.currentTime, currentSong.duration);
+    document.querySelector(".songTime").innerHTML = `${secondsToMinutesSeconds(
+      currentSong.currentTime
+    )}/${secondsToMinutesSeconds(currentSong.duration)}`;
+    document.querySelector(".circle").style.left =
+      (currentSong.currentTime / currentSong.duration) * 100 + "%";
+  });
+
+  //Add event listner to seekbar
+  document.querySelector(".seekbar").addEventListener("click", (e) => {
+    let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+    document.querySelector(".circle").style.left = percent + "%";
+    currentSong.currentTime = (currentSong.duration * percent) / 100;
+  });
+
+  //Add event listner to the hamburger
+  document.querySelector(".hamburger").addEventListener("click", () => {
+    document.querySelector(".left").style.left = 0;
+  });
+
+  // Add an event listener for close button
+  document.querySelector(".close").addEventListener("click", () => {
+    document.querySelector(".left").style.left = "-120%";
+  });
+
+  // Add an event listener to previous
+  previous.addEventListener("click", () => {
+    currentSong.pause();
+    songs.length;
+    console.log("Previous clicked");
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index - 1 >= 0) {
+      playMusic(songs[index - 1]);
+    } else {
+      playMusic(songs[songs.length - 1]);
+    }
+  });
+
+  // Add an event listener to next
+  next.addEventListener("click", () => {
+    currentSong.pause();
+    console.log("Next clicked");
+
+    let index = songs.indexOf(currentSong.src.split("/").slice(-1)[0]);
+    if (index + 1 < songs.length) {
+      playMusic(songs[index + 1]);
+    } else {
+      playMusic(songs[0]);
+    }
+  });
+
+  //Add an event listner to volume
+  document
+    .querySelector(".volume")
+    .getElementsByTagName("input")[0]
+    .addEventListener("change", (e) => {
+      currentSong.volume = parseInt(e.target.value) / 100;
+    });
+}
+
+main();
